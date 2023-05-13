eef44b4e205556623d9e10c3a25177b7c93c2940
Stepan <stsalamtat@gmail.com>
2023-05-13 16:31:58 +0300

    cherry-pick exel

--- mvc-spring/crud-spring-mvc/src/main/java/course/crud/spring/dao/PersonDAO.java
+++ mvc-spring/crud-spring-mvc/src/main/java/course/crud/spring/dao/PersonDAO.java
@@ -1,99 +1,60 @@
 package course.crud.spring.dao;
 
 import course.crud.spring.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 @Component
 public class PersonDAO {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final SessionFactory sessionFactory;
+
     @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
-
-
+    @Transactional(readOnly = true) // в методе помеченном этой аннотацией будет открыта транзакция(ей управляет Spring)
     public List<Person> index(){
-        return jdbcTemplate.query("SELECT * FROM person",
-                new BeanPropertyRowMapper<>(Person.class));
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("select p from Person p", Person.class).getResultList();
     }
 
+    @Transactional(readOnly = true)
     public Person show(int id){
         //query() - используем для запроса, чтобы получить данные
-        return jdbcTemplate.query("SELECT * FROM person WHERE id=?",
-                        new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny().orElse(null);
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Person.class, id);
     }
 
-    public Optional<Person> show(String email){
-        return jdbcTemplate.query("SELECT * FROM person WHERE email=?", new Object[] {email},
-                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
-    }
-
+    @Transactional
     public void save(Person person){
         // update - используется для обновления данных
-        jdbcTemplate.update("INSERT  INTO  person(name, age, email, address) VALUES(?, ?, ?, ?)",
-                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
     }
 
+    @Transactional
     public void update(int id, Person updatedPerson){
-        jdbcTemplate.update("UPDATE person SET name=?, age=?, email=?, adderess=? WHERE id=?",
-                updatedPerson.getName(), updatedPerson.getAge(),
-                updatedPerson.getEmail(), updatedPerson.getAddress(), id);
+        Session session = sessionFactory.getCurrentSession();
+
+        Person person = session.get(Person.class, id);
+        person.setName(updatedPerson.getName());
+        person.setAge(updatedPerson.getAge());
+        person.setEmail(updatedPerson.getEmail());
+
+        session.update(person);
     }
+
+    @Transactional
     public void delete(int id){
-        jdbcTemplate.update("DELETE  FROM person WHERE id=?", id);
-    }
-
-
-
-    public void batchUpdate(){
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-        jdbcTemplate.batchUpdate("INSERT INTO person VALUES(?, ?, ?, ?)",
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
-                        preparedStatement.setInt(1, people.get(i).getId());
-                        preparedStatement.setString(2, people.get(i).getName());
-                        preparedStatement.setInt(3, people.get(i).getAge());
-                        preparedStatement.setString(4, people.get(i).getEmail());
-                    }
-
-                    @Override
-                    public int getBatchSize() {
-                        return 1000;
-                    }
-                });
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time: " + (after - before));
-    }
-
-    public void getBdTest(){
-
-    }
-
-
-
-    // Этот метод нужен для теста batchUpdate
-    private List<Person> create1000People(){
-        List<Person> people = new ArrayList<>();
-
-        for(int i =0; i < 1000; i++){
-            people.add(new Person(i, "Name" + i, 30, "test" + i + "mail.ru", "some address"));
-        }
-        return people;
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Person.class, id));
     }
 }
