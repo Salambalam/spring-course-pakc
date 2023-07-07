197bd23b3b5573395b9e741a4c39819313f3d7b0
Stepan <stsalamtat@gmail.com>
2023-07-07 11:21:00 +0300

    Create security project

---
+++ spring-security/src/test/java/ru/chemakin/SecurityApp/SecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chemakin.SecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
