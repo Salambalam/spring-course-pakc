6b5ad0aea7ae8863d822e706bc63312f9d1e403b
Salambalam <stsalamtat@gmail.com>
2023-08-07 17:18:55 +0500

    comlete request for Kinopoisk API

--- RestApiConsumer/src/main/java/org/example/Translator.java
+++ RestApiConsumer/src/main/java/org/example/Translator.java
@@ -1,24 +1,24 @@
 package org.example;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Scanner;
 
 public class Translator {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String sentenceToTranslate = scanner.nextLine();
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
+        String url = "https://api.kinopoisk.dev/v1.3/movie?sortField=year&sortType=1&page=1&limit=3&name=Человек паук";
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.add("Authorization", "Bearer " + )
+        httpHeaders.add("X-API-KEY", "PCS47X6-DH7MC14-PMZ1EM6-GN8Q50N");
 
+        HttpEntity<?> requestEntity = new HttpEntity<>(httpHeaders);
 
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        String response = responseEntity.toString();
+
+        System.out.println(response);
     }
 }
