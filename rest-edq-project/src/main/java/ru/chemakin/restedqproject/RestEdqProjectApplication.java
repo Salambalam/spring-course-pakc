3d0f46d4e7a7458f259aa41da009da14aafb5000
Salambalam <stsalamtat@gmail.com>
2023-08-11 17:24:39 +0500

    add FirstRESTController

---
+++ rest-edq-project/src/main/java/ru/chemakin/restedqproject/RestEdqProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.chemakin.restedqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEdqProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEdqProjectApplication.class, args);
+	}
+
+}
